b4634b900ba59f8d51913674dd1606ac665983f3
c.morais <c.morais1507@gmail.com>
2023-01-24 18:14:41 -0300

    feat: Add styles in Card

--- components/ProdCard/index.tsx
+++ components/ProdCard/index.tsx
@@ -7,13 +7,16 @@ const ProdCard = () => {
     <>
       <div className="card w-96 bg-base-100 shadow xl mb-10 border-2 border-neutral">
         <figure>
-          <img src="https://i.ibb.co/qJL6pyq/Design-sem-nome.png" alt="Design-sem-nome" />
+          <a href="#"><img src="https://i.ibb.co/qJL6pyq/Design-sem-nome.png" alt="Design-sem-nome" className='hover:brightness-50 transition-all duration-150'/></a>
         </figure>
         <div className='card-body'>
           <h2 className="card-title text-2xl">Produto 1</h2>
           <div className='flex'>
             <h1 className={`mb-2 ${(prod__dprice > prod__nprice) ? "line-through text-error" : "text-info"}`}>R${prod__dprice.toFixed(2)}</h1>
-            <h1 className={`ml-3 text-green-600 no-underline ${(prod__dprice > prod__nprice) ? "block" : "hidden"}`}>R${prod__nprice.toFixed(2)} <span className='badge badge-secondary p-2'>Poupe {promo__value}%</span></h1>
+            <h1 className={`ml-3 text-green-600 no-underline ${(prod__dprice > prod__nprice) ? "block" : "hidden"}`}>
+              R${prod__nprice.toFixed(2)}
+              <span className='badge badge-secondary p-2 ml-2'>Poupe {promo__value.toFixed(0)}%</span>
+            </h1>
           </div>
           <div className="card-actions justify-around">
             <button className="btn btn-secondary btn-square"><BsFillStarFill className='text-2xl'/></button>
