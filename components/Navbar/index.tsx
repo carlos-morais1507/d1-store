ecbe5b693a95480f4c72087750e388dfcf6116e3
c.morais <c.morais1507@gmail.com>
2023-01-23 21:30:09 -0300

    feat: Add Navbar (Not responsive yet)

--- components/Navbar/index.tsx
+++ components/Navbar/index.tsx
@@ -1,19 +1,64 @@
 import Link from "next/link"
-import { BsFillCartFill } from 'react-icons/bs'
+import { BsFillCartFill, BsSearch } from 'react-icons/bs'
+import { Unbounded } from "@next/font/google"
+import { prod__count, price__total, logged } from "@/store/StoreVars"
+import Image from "next/image"
+
+const unbounded = Unbounded({
+  weight: '800',
+  subsets: ['latin'],
+})
 
 const Navbar = () => {
   return (
-    <div className="navbar bg-base-200">
-      <div className="flex-1">
+    <div className="navbar border-b-neutral fixed border-b-2">
+      <div className="flex-1 LOGO">
         <Link href="/" className="btn btn-ghost normal-case text-xl">
-          D1Store
+          <h1 className={`${unbounded.className} font-black`}>D1STORE</h1>
         </Link>
       </div>
-      <div className="flex-none">
-        <BsFillCartFill />
+      <div className="flex-none gap-2 SEARCH">
+        <div className="form-control">
+          <input type="text" placeholder={`Pesquisar...`} className="input bg-base-200"/>
+        </div>
+      </div>
+      <div className="flex-none mx-3 CART"> 
+        <div className="dropdown dropdown-bottom dropdown-end">
+          <label tabIndex={0} className="btn btn-circle btn-ghost">
+            <div className="indicator">
+              <BsFillCartFill className="text-xl"/>
+              <span className="badge badge-sm indicator-item bg-primary text-inherit">{prod__count}</span>
+            </div>      
+          </label>
+          <div className="mt-3 card card-compact dropdown-content bg-base-100 w-56 drop-shadow-xl border-2 border-neutral ">
+            <div className="card-body">
+              <span className="font-bold text-lg">{prod__count} {(prod__count != 1) ? "Itens" : "Item"}</span>
+              <span className="text-info">Total: R${price__total.toFixed(2)}</span>
+              <div className="card-actions">
+                <Link href="/Cart" className="btn btn-primary btn-block">Ir ao Carrinho</Link>
+              </div>
+            </div>
+          </div>         
+        </div>
+      </div>
+      <div className="flex-none PROFILE">
+        <div className="dropdown dropdown-botom dropdown-end">
+          <label tabIndex={0} className="btn btn-ghost btn-circle avatar">
+            <div className="w-10 rounded-full">
+              <img src="https://placeimg.com/80/80/people" alt="Avatar" />
+            </div>
+          </label>
+          <ul tabIndex={0} className="menu menu-compact dropdown-content mt-3 p-2 drop-shadow-xl border-2 border-neutral bg-base-100 rounded-box w-52">
+            <li className={(logged) ? "block" : "hidden"}><a className="justify-between">Meu Perfil</a></li>
+            <li className={(logged) ? "block" : "hidden"}><a>Configurações</a></li>
+            <li  className={`${(logged) ? "block" : "hidden"} text-error`}><a>Logout</a></li>
+            <li  className={`${(logged) ? "hidden" : "block"} text-success`}><a>Login</a></li>            
+          </ul>
+        </div>
       </div>
     </div>
   )
 }
 
 export default Navbar
+
---
+++ pages/Cart/index.tsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import { Inter } from '@next/font/google'
+import Head from 'next/head'
+
+const inter = Inter({ subsets: ['latin'] })
+
+const Cart = () => {
+  return (
+    <>
+      <Head>
+        <title>D1 - Carrinho</title>
+      </Head>
+      <main className={`${inter.className} mt-[66px]`}>
+        Cart
+      </main>
+    </>
+  )
+}
+
+export default Cart
