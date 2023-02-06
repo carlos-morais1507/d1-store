0bf268d704e1018b8ca3657d3b4e9566d6e18e29
c.morais <c.morais1507@gmail.com>
2023-02-06 13:01:41 -0300

    feat: Start to develop Cart Page

--- pages/Cart/index.tsx
+++ pages/Cart/index.tsx
@@ -16,8 +16,42 @@ const Cart = () => {
       <Head>
         <title>D1 - Carrinho</title>
       </Head>
-      <main className={`${inter.className} sm:mt-[66px]`}>
-        Cart
+      <main className={`${inter.className} sm:mt-[66px] bg-base-200`}>
+        <div className='flex items-center justify-center'>
+          <div className='flex justify-start flex-col'>
+            <h1 className='font-bold text-lg'>Informações de Entrega</h1>
+            <div className='bg-base-100 border-2 border-neutral p-4 rounded-xl'>
+              <div className="flex gap-3">
+                <div>
+                  <label className='label'>
+                    <span className='label-text'>Nome</span>
+                  </label>
+                  <input type="text" placeholder='Digite aqui' className='input input-bordered w-full max-w-xs'/>
+                </div>
+                <div>
+                  <label className='label'>
+                    <span className='label-text'>Telefone</span>
+                  </label>
+                  <input type="tel" placeholder='Digite aqui' className='input input-bordered w-full max-w-xs'/>
+                </div>
+              </div>
+              <div className="flex gap-3">
+                <div>
+                  <label className='label'>
+                    <span className='label-text'>Email</span>
+                  </label>
+                  <input type="email" placeholder='Digite aqui' className='input input-bordered w-full max-w-xs'/>
+                </div>
+                <div>
+                  <label className='label'>
+                    <span className='label-text'>Cidade</span>
+                  </label>
+                  <input type="text" placeholder='Digite aqui' className='input input-bordered w-full max-w-xs'/>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
       </main>
     </>
   )
